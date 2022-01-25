a76125369efe9034bc84556b46f487775ffe334c
aesirteam <zhongkui@139.com>
2022-01-25 15:48:00 +0800

    added: done.sh

---
+++ done.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+set -euxo pipefail
+
+# configure apt for non-interactive mode.
+export DEBIAN_FRONTEND=noninteractive
+
+sed 's,\\,\\\\,g' >/etc/issue <<'EOF'
+
+     _ __  _ __ _____  ___ __ ___   _____  __ __   _____
+    | '_ \| '__/ _ \ \/ / '_ ` _ \ / _ \ \/ / \ \ / / _ \
+    | |_) | | | (_) >  <| | | | | | (_) >  <   \ V /  __/
+    | .__/|_|  \___/_/\_\_| |_| |_|\___/_/\_\   \_/ \___|
+    | |
+    |_|
+
+EOF
+
+# disable the "You do not have a valid subscription for this server. Please visit www.proxmox.com to get a list of available options."
+# message that appears each time you logon the web-ui.
+# NB this file is restored when you (re)install the pve-manager package.
+echo 'Proxmox.Utils.checked_command = function(o) { o(); };' >>/usr/share/pve-manager/js/pvemanagerlib.js
+
+# configure the shell.
+cat >/etc/profile.d/login.sh <<'EOF'
+[[ "$-" != *i* ]] && return
+export EDITOR=vim
+export PAGER=less
+alias l='ls -lF --color'
+alias ll='l -a'
+alias h='history 25'
+alias j='jobs -l'
+EOF
+
+cat >/etc/inputrc <<'EOF'
+set input-meta on
+set output-meta on
+set show-all-if-ambiguous on
+set completion-ignore-case on
+"\e[A": history-search-backward
+"\e[B": history-search-forward
+"\eOD": backward-word
+"\eOC": forward-word
+EOF
+
+# configure the motd.
+# NB this was generated at http://patorjk.com/software/taag/#p=display&f=Big&t=proxmox%20ve.
+#    it could also be generated with figlet.org.
+cat >/etc/motd <<'EOF'
+
+     _ __  _ __ _____  ___ __ ___   _____  __ __   _____
+    | '_ \| '__/ _ \ \/ / '_ ` _ \ / _ \ \/ / \ \ / / _ \
+    | |_) | | | (_) >  <| | | | | | (_) >  <   \ V /  __/
+    | .__/|_|  \___/_/\_\_| |_| |_|\___/_/\_\   \_/ \___|
+    | |
+    |_|
+
+EOF
+
+
+echo y | pveceph install --version octopus
+touch /etc/ceph/ceph.conf
